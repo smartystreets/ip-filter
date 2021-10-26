ef6e8a6d87af127aeb776868b9460ab190f30237
Caroline Hickey <caroline@smartystreets.com>
2021-10-26 15:32:53 -0600

    started Restructuring the tree

--- tree.go
+++ tree.go
@@ -29,136 +29,176 @@ func (this *treeNode) New(addresses []string) {
 	}
 }
 
-func (this *treeNode) Insert(IPAddress string) error {
-	if len(IPAddress) == 0 {
+func (this *treeNode) Insert(ipAddress string) error {
+	//TODO: I think you need to take the subnetBits out of it... just add every possibility to the tree
+	// however, you will need to know the subnetBits to do the math to find the ranges
+	var cRange bool
+	var ipFragment uint64
+	position := strings.Index(ipAddress, "/")
+	subnetBits, _ := strconv.Atoi(ipAddress[position+1:])
+	ipAddress = ipAddress[:position]
+
+	if subnetBits > 32 {
 		return ErrInvalidIPAddress
 	}
+	indexes := findIndexes(ipAddress)
 
-	var IPFragment string
-	if position := strings.Index(IPAddress, "/"); position != -1 {
-
-		subnetBits, _ := strconv.Atoi(IPAddress[position+1:])
-
+	if subnetBits > 7 && subnetBits < 16 {
 		if subnetBits == 8 {
-			IPFragment = IPAddress[:strings.Index(IPAddress, ".")]
-			this.addNetworkChild(0, 0, IPFragment)
-			return nil
+			cRange = true
 		}
-		if subnetBits == 16 {
-			index := findIndex(IPAddress, 16)
-			IPFragment = IPAddress[:index]
-			this.addNetworkChild(0, 0, IPFragment)
-			return nil
-		}
-		if subnetBits == 24 {
-			index := findIndex(IPAddress, 24)
-			IPFragment = IPAddress[:index]
-			this.addNetworkChild(0, 0, IPFragment)
-			return nil
-		}
-		if subnetBits == 32 {
-			IPFragment = IPAddress[:position]
-			this.addNetworkChild(0, 0, IPFragment)
-			return nil
-		}
-		IPAddress = IPAddress[:position]
-
-		if subnetBits > 8 && subnetBits < 16 {
-			firstSection := strings.Index(IPAddress, ".")
-			secondSection := findIndex(IPAddress, 16)
-
-			minValue, _ := strconv.ParseUint(IPAddress[firstSection+1:secondSection], 10, 64)
-
-			changeableBits := 16 - subnetBits
-
-			valueToAdd := powInt(2, changeableBits) - 1
-
-			maxValue := minValue + valueToAdd
-
-			IPFragment = IPAddress[:strings.Index(IPAddress, ".")]
-
-			this.addNetworkChild(maxValue, minValue, IPFragment)
-			return nil
-		}
-		if subnetBits > 16 && subnetBits < 24 {
-			firstSection := findIndex(IPAddress, 16)
-			secondSection := findIndex(IPAddress, 24)
-			minValue, _ := strconv.ParseUint(IPAddress[firstSection+1:secondSection], 10, 64)
-
-			changeableBits := 24 - subnetBits
-
-			valueToAdd := powInt(2, changeableBits) - 1
-
-			maxValue := minValue + valueToAdd
-
-			IPFragment = IPAddress[:findIndex(IPAddress, 16)]
-
-			this.addNetworkChild(maxValue, minValue, IPFragment)
-			return nil
-		}
-		if subnetBits > 24 && subnetBits < 32 {
-			minValue, _ := strconv.ParseUint(IPAddress[findIndex(IPAddress, 24)+1:], 10, 64)
-
-			changeableBits := 32 - subnetBits
-
-			valueToAdd := powInt(2, changeableBits) - 1
-
-			maxValue := minValue + valueToAdd
-
-			IPFragment = IPAddress[:findIndex(IPAddress, 24)]
-
-			this.addNetworkChild(maxValue, minValue, IPFragment)
-			return nil
-		}
-
+		ipFragment,_ := strconv.ParseUint(ipAddress[:indexes[0]], 10, 64)
 	}
+	if subnetBits > 15 && subnetBits < 24 {
+		if subnetBits == 16 {
+			cRange = true
+		}
+		ipFragment,_ := strconv.ParseUint(ipAddress[:indexes[0]], 10, 64)
+	}
+	if subnetBits > 23 && subnetBits < 32 {
+		if subnetBits == 24 {
+			cRange = true
+		}
+		ipFragment,_ := strconv.ParseUint(ipAddress[:indexes[0]], 10, 64)
+	}
+	if subnetBits == 32 {
+		cRange == true
+		ipFragment,_ := strconv.ParseUint(ipAddress[:indexes[0]], 10, 64)
+	}
+
+	for _, child := range this.children{
+		if child.value != ipFragment{
+			continue
+		}
+	}
+	//if len(IPAddress) == 0 {
+	//	return ErrInvalidIPAddress
+	//}
+	//
+	//var IPFragment string
+	//if position := strings.Index(IPAddress, "/"); position != -1 {
+	//
+	//	subnetBits, _ := strconv.Atoi(IPAddress[position+1:])
+	//
+	//	if subnetBits == 8 {
+	//		IPFragment = IPAddress[:strings.Index(IPAddress, ".")]
+	//		this.addNetworkChild(0, 0, IPFragment)
+	//		return nil
+	//	}
+	//	if subnetBits == 16 {
+	//		index := findIndex(IPAddress, 16)
+	//		IPFragment = IPAddress[:index]
+	//		this.addNetworkChild(0, 0, IPFragment)
+	//		return nil
+	//	}
+	//	if subnetBits == 24 {
+	//		index := findIndex(IPAddress, 24)
+	//		IPFragment = IPAddress[:index]
+	//		this.addNetworkChild(0, 0, IPFragment)
+	//		return nil
+	//	}
+	//	if subnetBits == 32 {
+	//		IPFragment = IPAddress[:position]
+	//		this.addNetworkChild(0, 0, IPFragment)
+	//		return nil
+	//	}
+	//	IPAddress = IPAddress[:position]
+	//
+	//	if subnetBits > 8 && subnetBits < 16 {
+	//		firstSection := strings.Index(IPAddress, ".")
+	//		secondSection := findIndex(IPAddress, 16)
+	//
+	//		minValue, _ := strconv.ParseUint(IPAddress[firstSection+1:secondSection], 10, 64)
+	//
+	//		changeableBits := 16 - subnetBits
+	//
+	//		valueToAdd := powInt(2, changeableBits) - 1
+	//
+	//		maxValue := minValue + valueToAdd
+	//
+	//		IPFragment = IPAddress[:strings.Index(IPAddress, ".")]
+	//
+	//		this.addNetworkChild(maxValue, minValue, IPFragment)
+	//		return nil
+	//	}
+	//	if subnetBits > 16 && subnetBits < 24 {
+	//		firstSection := findIndex(IPAddress, 16)
+	//		secondSection := findIndex(IPAddress, 24)
+	//		minValue, _ := strconv.ParseUint(IPAddress[firstSection+1:secondSection], 10, 64)
+	//
+	//		changeableBits := 24 - subnetBits
+	//
+	//		valueToAdd := powInt(2, changeableBits) - 1
+	//
+	//		maxValue := minValue + valueToAdd
+	//
+	//		IPFragment = IPAddress[:findIndex(IPAddress, 16)]
+	//
+	//		this.addNetworkChild(maxValue, minValue, IPFragment)
+	//		return nil
+	//	}
+	//	if subnetBits > 24 && subnetBits < 32 {
+	//		minValue, _ := strconv.ParseUint(IPAddress[findIndex(IPAddress, 24)+1:], 10, 64)
+	//
+	//		changeableBits := 32 - subnetBits
+	//
+	//		valueToAdd := powInt(2, changeableBits) - 1
+	//
+	//		maxValue := minValue + valueToAdd
+	//
+	//		IPFragment = IPAddress[:findIndex(IPAddress, 24)]
+	//
+	//		this.addNetworkChild(maxValue, minValue, IPFragment)
+	//		return nil
+	//	}
+	//
+	//}
+	//return nil
 	return nil
 }
 
 func (this *treeNode) addNetworkChild(maxValue, minValue uint64, IPFragment string) error {
-
-	for _, children := range this.children {
-		if children.value == IPFragment {
-			for _, childrenRanges := range children.children {
-				if childrenRanges.minValue == minValue && childrenRanges.maxValue == maxValue {
-					return nil
-				}
-				children.addMinMaxChild(maxValue, minValue)
-				return nil
-			}
-		}
-	}
-
-	child := &treeNode{
-		value:    IPFragment,
-		children: nil,
-	}
-
-	this.children = append(this.children, child)
-
-	if maxValue == 0 {
-		return nil
-	}
-
-	child.addMinMaxChild(maxValue, minValue)
+	//
+	//for _, children := range this.children {
+	//	if children.value == IPFragment {
+	//		for _, childrenRanges := range children.children {
+	//			if childrenRanges.minValue == minValue && childrenRanges.maxValue == maxValue {
+	//				return nil
+	//			}
+	//			children.addMinMaxChild(maxValue, minValue)
+	//			return nil
+	//		}
+	//	}
+	//}
+	//
+	//child := &treeNode{
+	//	value:    IPFragment,
+	//	children: nil,
+	//}
+	//
+	//this.children = append(this.children, child)
+	//
+	//if maxValue == 0 {
+	//	return nil
+	//}
+	//
+	//child.addMinMaxChild(maxValue, minValue)
 	return nil
 }
 func (this *treeNode) addMinMaxChild(maxValue, minValue uint64) error {
-
-	for _, children := range this.children {
-		if children.minValue == minValue && children.maxValue == maxValue {
-			return nil
-		}
-	}
-
-	child := &treeNode{
-		minValue: minValue,
-		maxValue: maxValue,
-		children: nil,
-	}
-
-	this.children = append(this.children, child)
-
+	//for _, children := range this.children {
+	//	if children.minValue == minValue && children.maxValue == maxValue {
+	//		return nil
+	//	}
+	//}
+	//
+	//child := &treeNode{
+	//	minValue: minValue,
+	//	maxValue: maxValue,
+	//	children: nil,
+	//}
+	//
+	//this.children = append(this.children, child)
 	return nil
 }
 
@@ -242,54 +282,6 @@ func (this *treeNode) Search(IPAddress string) bool {
 	//		}
 	//	}
 	//}
-	var childRange string
-	var fragment string
-
-	indexes := findIndexes(IPAddress)
-
-	for i := 3; i >= 0; i-- {
-
-		if i == 3 {
-			fragment = IPAddress
-		} else {
-			fragment = IPAddress[:indexes[i]]
-		}
-
-		for _, child := range this.children {
-
-			if child.value != fragment {
-				continue
-			}
-
-			if child.children == nil {
-				return true
-			}
-
-			length := len(fragment)
-			firstSection := length + 1
-			var secondSection int
-
-			if length == indexes[2] {
-				secondSection = len(IPAddress)
-			}
-			if length == indexes[1] {
-				secondSection = indexes[2]
-			}
-			if length == indexes[0] {
-				secondSection = indexes[1]
-			}
-
-			childRange = IPAddress[firstSection:secondSection]
-
-			childValue, _ := strconv.ParseUint(childRange, 10, 64)
-
-			for _, c := range child.children {
-				if childValue >= c.minValue && childValue <= c.maxValue {
-					return true
-				}
-			}
-		}
-	}
 	return false
 }
 
@@ -306,261 +298,3 @@ func findIndexes(IPAddress string) [3]int {
 	}
 	return indexes
 }
-
-//var StringNumberMap = map[string]uint64{
-//	"1":   1,
-//	"2":   2,
-//	"3":   3,
-//	"4":   4,
-//	"5":   5,
-//	"6":   6,
-//	"7":   7,
-//	"8":   8,
-//	"9":   9,
-//	"10":  10,
-//	"11":  11,
-//	"12":  12,
-//	"13":  13,
-//	"14":  14,
-//	"15":  15,
-//	"16":  16,
-//	"17":  17,
-//	"18":  18,
-//	"19":  19,
-//	"20":  20,
-//	"21":  21,
-//	"22":  22,
-//	"23":  23,
-//	"24":  24,
-//	"25":  25,
-//	"26":  26,
-//	"27":  27,
-//	"28":  28,
-//	"29":  29,
-//	"30":  30,
-//	"31":  31,
-//	"32":  32,
-//	"33":  33,
-//	"34":  34,
-//	"35":  35,
-//	"36":  36,
-//	"37":  37,
-//	"38":  38,
-//	"39":  39,
-//	"40":  40,
-//	"41":  41,
-//	"42":  42,
-//	"43":  43,
-//	"44":  44,
-//	"45":  45,
-//	"46":  46,
-//	"47":  47,
-//	"48":  48,
-//	"49":  49,
-//	"50":  50,
-//	"51":  51,
-//	"52":  52,
-//	"53":  53,
-//	"54":  54,
-//	"55":  55,
-//	"56":  56,
-//	"57":  57,
-//	"58":  58,
-//	"59":  59,
-//	"60":  60,
-//	"61":  61,
-//	"62":  62,
-//	"63":  63,
-//	"64":  64,
-//	"65":  65,
-//	"66":  66,
-//	"67":  67,
-//	"68":  68,
-//	"69":  69,
-//	"70":  70,
-//	"71":  71,
-//	"72":  72,
-//	"73":  73,
-//	"74":  74,
-//	"75":  75,
-//	"76":  76,
-//	"77":  77,
-//	"78":  78,
-//	"79":  79,
-//	"80":  80,
-//	"81":  81,
-//	"82":  82,
-//	"83":  83,
-//	"84":  84,
-//	"85":  85,
-//	"86":  86,
-//	"87":  87,
-//	"88":  88,
-//	"89":  89,
-//	"90":  90,
-//	"91":  91,
-//	"92":  92,
-//	"93":  93,
-//	"94":  94,
-//	"95":  95,
-//	"96":  96,
-//	"97":  97,
-//	"98":  98,
-//	"99":  99,
-//	"100": 100,
-//	"101": 101,
-//	"102": 102,
-//	"103": 103,
-//	"104": 104,
-//	"105": 105,
-//	"106": 106,
-//	"107": 107,
-//	"108": 108,
-//	"109": 109,
-//	"110": 110,
-//	"111": 111,
-//	"112": 112,
-//	"113": 113,
-//	"114": 114,
-//	"115": 115,
-//	"116": 116,
-//	"117": 117,
-//	"118": 118,
-//	"119": 119,
-//	"120": 120,
-//	"121": 121,
-//	"122": 122,
-//	"123": 123,
-//	"124": 124,
-//	"125": 125,
-//	"126": 126,
-//	"127": 127,
-//	"128": 128,
-//	"129": 129,
-//	"130": 130,
-//	"131": 131,
-//	"132": 132,
-//	"133": 133,
-//	"134": 134,
-//	"135": 135,
-//	"136": 136,
-//	"137": 137,
-//	"138": 138,
-//	"139": 139,
-//	"140": 140,
-//	"141": 141,
-//	"142": 142,
-//	"143": 143,
-//	"144": 144,
-//	"145": 145,
-//	"146": 146,
-//	"147": 147,
-//	"148": 148,
-//	"149": 149,
-//	"150": 150,
-//	"151": 151,
-//	"152": 152,
-//	"153": 153,
-//	"154": 154,
-//	"155": 155,
-//	"156": 156,
-//	"157": 157,
-//	"158": 158,
-//	"159": 159,
-//	"160": 160,
-//	"161": 161,
-//	"162": 162,
-//	"163": 163,
-//	"164": 164,
-//	"165": 165,
-//	"166": 166,
-//	"167": 167,
-//	"168": 168,
-//	"169": 169,
-//	"170": 170,
-//	"171": 171,
-//	"172": 172,
-//	"173": 173,
-//	"174": 174,
-//	"175": 175,
-//	"176": 176,
-//	"177": 177,
-//	"178": 178,
-//	"179": 179,
-//	"180": 180,
-//	"181": 181,
-//	"182": 182,
-//	"183": 183,
-//	"184": 184,
-//	"185": 185,
-//	"186": 186,
-//	"187": 187,
-//	"188": 188,
-//	"189": 189,
-//	"190": 190,
-//	"191": 191,
-//	"192": 192,
-//	"193": 193,
-//	"194": 194,
-//	"195": 195,
-//	"196": 196,
-//	"197": 197,
-//	"198": 198,
-//	"199": 199,
-//	"200": 200,
-//	"201": 201,
-//	"202": 202,
-//	"203": 203,
-//	"204": 204,
-//	"205": 205,
-//	"206": 206,
-//	"207": 207,
-//	"208": 208,
-//	"209": 209,
-//	"210": 210,
-//	"211": 211,
-//	"212": 212,
-//	"213": 213,
-//	"214": 214,
-//	"215": 215,
-//	"216": 216,
-//	"217": 217,
-//	"218": 218,
-//	"219": 219,
-//	"220": 220,
-//	"221": 221,
-//	"222": 222,
-//	"223": 223,
-//	"224": 224,
-//	"225": 225,
-//	"226": 226,
-//	"227": 227,
-//	"228": 228,
-//	"229": 229,
-//	"230": 230,
-//	"231": 231,
-//	"232": 232,
-//	"233": 233,
-//	"234": 234,
-//	"235": 235,
-//	"236": 236,
-//	"237": 237,
-//	"238": 238,
-//	"239": 239,
-//	"240": 240,
-//	"241": 241,
-//	"242": 242,
-//	"243": 243,
-//	"244": 244,
-//	"245": 245,
-//	"246": 246,
-//	"247": 247,
-//	"248": 248,
-//	"249": 249,
-//	"250": 250,
-//	"251": 251,
-//	"252": 252,
-//	"253": 253,
-//	"254": 254,
-//	"255": 255,
-//}
