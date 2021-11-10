4e51542955535757cd2e5ab46d228a51ef0b0924
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-11-09 19:46:11 -0700

    Extracted constants.

--- tree.go
+++ tree.go
@@ -50,12 +50,12 @@ func (this *treeNode) add(subnetMask string) {
 
 	current := this
 	for i := 0; i < subnetBits; i++ {
-		next := uint32(numericIP << (i) >> (31))
-		child := current.children[next]
+		nextBit := uint32(numericIP << i >> ipv4BitMask)
+		child := current.children[nextBit]
 
 		if child == nil {
 			child = newNode()
-			current.children[next] = child
+			current.children[nextBit] = child
 		}
 		current = child
 	}
@@ -73,20 +73,18 @@ func isNumeric(value string) bool {
 }
 
 func (this *treeNode) Contains(ipAddress string) bool {
-	var numericIP uint32
-
 	if len(ipAddress) == 0 {
 		return false
 	}
 
+	var numericIP uint32
 	if numericIP = parseIPAddress(ipAddress); numericIP == 0 {
 		return false
 	}
 
 	current := this
-
-	for i := 0; i < 32; i++ {
-		nextBit := uint32(numericIP << (i) >> (31))
+	for i := 0; i < ipv4BitCount; i++ {
+		nextBit := uint32(numericIP << i >> ipv4BitMask)
 		child := current.children[nextBit]
 
 		if child == nil {
@@ -98,44 +96,55 @@ func (this *treeNode) Contains(ipAddress string) bool {
 			return true
 		}
 	}
+
 	return false
 }
 func parseIPAddress(value string) uint32 {
 	var numericIP uint32
 	var count int
 
-	for i := 0; i < 4; i++ {
+	for i := 0; i < octetCount; i++ {
 		var fragment uint64
 		var index int
 
 		for x := range value {
-			if value[x] == '.' {
+			if value[x] == octetSeparator {
 				index = x
 				count++
 				break
 			}
+
 			continue
 		}
 
 		if index == 0 {
-			fragment, _ = strconv.ParseUint(value, 10, 32)
+			fragment, _ = strconv.ParseUint(value, decimalNumber, ipv4BitCount)
 		} else {
-			fragment, _ = strconv.ParseUint(value[:index], 10, 32)
+			fragment, _ = strconv.ParseUint(value[:index], decimalNumber, ipv4BitCount)
 		}
 
 		value = value[index+1:]
-
-		if len(value) == 0 && count < 3 {
+		if len(value) == 0 && count < octetSeparatorCount {
 			return 0
 		}
 
-		numericIP = numericIP << 8
+		numericIP = numericIP << octetBits
 		numericIP += uint32(fragment)
 	}
 
-	if count > 3 || count < 3 {
+	if count > octetSeparatorCount || count < octetSeparatorCount {
 		return 0
 	}
 
 	return numericIP
 }
+
+const (
+	decimalNumber       = 10
+	ipv4BitCount        = 32
+	ipv4BitMask         = ipv4BitCount - 1
+	octetBits           = 8
+	octetSeparator      = '.'
+	octetSeparatorCount = 3
+	octetCount          = 4
+)
